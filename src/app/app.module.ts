06c5dff2e1d7f8676b4b803296be8a9c548f5bba
Your Name <you@example.com>
2017-11-23 18:59:31 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap';
+
+import { HomePageModule } from './modules/home/module';
+import { OrdersModule } from './modules/orders/module';
+import { ProductsModule } from './modules/products/module';
+import { UserModule } from './modules/user/module';
+import { HeaderComponent } from './modules/core/header/header';
+import { FooterComponent } from './modules/core/footer/footer';
+import { ROUTING } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    AlertModule.forRoot(),
+  	ROUTING,
+    BrowserModule,
+    HomePageModule,
+    OrdersModule,
+    ProductsModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
